b9c376c0905eb11166811b9a8426a2d860997f57
HoangHai <hoanghai18497@gmail.com>
2022-01-01 16:20:48 +0700

    initial

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require("express");
+var app = express();
+
+app.use(express.static(__dirname + '/public/'));
+app.set("view engine", "ejs");
+app.get("/", function (req, res){
+    res.render('pages/index');
+});
+
+var port = process.argv[2] || 6789;
+app.listen(port);
+console.log("Listening to port: ", port);
