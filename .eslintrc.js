1868cbdec0194929534ac26b187269a4ce659b55
hainh <hoanghai18497@gmail.com>
2022-11-27 00:43:54 +0700

    init eslint

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es2021": true
+    },
+    "extends": "eslint:recommended",
+    "overrides": [
+    ],
+    "parserOptions": {
+        "ecmaVersion": "latest"
+    },
+    "ignorePatterns": [
+        "three.js",
+        "**/three/*.js",
+        "**/three/*/*.js",
+        "**/jquery/*.js"
+    ],
+    "rules": {
+    }
+}
